34c9906804503d2c852ba5a8ee034817b3c55bc1
sunil kumar rao s <sunilkumarraos007@gmail.com>
2021-07-13 02:01:18 +0530

    prod DB

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ app.use(cors())
 
 
 mongoose.connect('mongodb+srv://pavan:Pavan123@cluster0.bcifb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+
+
+mongoose.connect('mongodb+srv://admin:vechicle123@cluster0.z047r.mongodb.net/electric?retryWrites=true&w=majority', { useNewUrlParser: true });
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, '\n connection error:'));
 db.once('open', () => {console.log('db connected successfully');});
